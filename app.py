43a13504d9f375b0894908c9690ce28c5425511c
mangwu <1185956753@qq.com>
2020-07-08 18:48:44 +0800

    database uprade

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import click
 import os
 import sys
-
-import click
+from flask import Flask
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 
 # SQLite URI compatible
 WIN = sys.platform.startswith('win')
@@ -11,31 +11,29 @@ if WIN:
     prefix = 'sqlite:///'
 else:
     prefix = 'sqlite:////'
-
-
+	
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
 
-
-@app.cli.command()
-@click.option('--drop', is_flag=True, help='Create after drop.')
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
 def initdb(drop):
     """Initialize the database."""
-    if drop:
+    if drop:  # 判断是否输入了选项
         db.drop_all()
     db.create_all()
-    click.echo('Initialized database.')
-
+    click.echo('Initialized database.')  # 输出提示信息
 
 @app.cli.command()
 def forge():
     """Generate fake data."""
     db.create_all()
 
-    name = 'mangwu'
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
     movies = [
         {'title': 'My Neighbor Totoro', 'year': '1988'},
         {'title': 'Dead Poets Society', 'year': '1989'},
@@ -57,13 +55,11 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-
-
+	
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+	
 name = 'Grey Li'
 movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
@@ -77,27 +73,18 @@ movies = [
     {'title': 'WALL-E', 'year': '2008'},
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
+
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
 
-
 @app.route('/')
+def hello():
+    return 'Welcome to My Watchlist!'
+@app.route('/index')	
 def index():
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
     return render_template('index.html', name=name, movies=movies)
-    user = User.query.first()
-    movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
-#主页
-@app.route('/home')
-def home():
-    return render_template('home.html')
 	
-
-
-#用户
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User page. User: %s' % escape(name)
-
