d9050ae04ae19acf8456d60fb75b4c16bfea3b0f
mangwu <1185956753@qq.com>
2020-07-08 12:02:51 +0800

    index

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import url_for, escape
 app = Flask(__name__)
 
 
-
+name = 'mangwu'
+citys = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 #主页
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif" alt = "totoro">'
+def index():
+    return render_template('index.html', name=name, citys=citys)
 	
 #用户
 @app.route('/user/<name>')
@@ -17,14 +29,3 @@ def user_page(name):
     return 'User page. User: %s' % escape(name)
 
 #	
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 输出：/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='mangwu'))  # 输出：/user/mangwu
-    print(url_for('user_page', name='peter'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-    return 'Test page'
